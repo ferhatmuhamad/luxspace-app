172faa73c35bd76d25113ae26da10d6d2b18d426
ferhatmuhamad <ferhatmuhamad221@gmail.com>
2025-04-21 17:15:30 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-blue-300">
+        <h1 className="text-3xl font-bold text-yellow-500 underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
